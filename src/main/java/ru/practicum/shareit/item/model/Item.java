0cecf095bb5515cb382c68818ddf9b37ebef0d02
Ruslan Pavlov <vtb70191685@corp.dev.vtb>
2025-08-17 23:50:56 +0300

    Правка, убрал лишний перенос.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -16,7 +16,6 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
